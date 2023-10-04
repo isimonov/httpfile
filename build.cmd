e52bc08eaf4731e095f3c50c314be928090f3248
Ivan Simonov <isimonov@sberbank-tele.com>
2023-10-04 22:20:06 +0300

    fix: build.cmd

--- build.cmd
+++ build.cmd
@@ -1,9 +1,11 @@
+for %%a in (".") do set CURRENT_DIR_NAME=%%~na
+
 if not exist "./build" mkdir ./build
 
 set GOOS=linux
 set GOARCH=amd64
-go build -o ./build/httpfile .
+go build -o ./build/%CURRENT_DIR_NAME% .
 
 set GOOS=windows
 set GOARCH=amd64
-go build -o ./build/httpfile.exe .
+go build -o ./build/%CURRENT_DIR_NAME%.exe .
